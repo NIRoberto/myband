e51ce037fcb49bf9c577ce5085c005b0e9c7f622
Robert NIYITANGA <robertwilly668@gmail.com>
2020-11-02 11:33:45 +0200

    * create a blog post

--- server/routes/blogpost.js
+++ server/routes/blogpost.js
@@ -10,6 +10,7 @@ const router = express.Router();
 // view all blopos
 router.get('/blogs', blogcontroller.findAll)
 
-
+// create one
+router.post('/blog', image, blogcontroller.postOne);
 
 export default router;
